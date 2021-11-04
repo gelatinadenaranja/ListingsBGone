6146e600daf837c7642a2eb271f04fd401fd3f93
ndeleeuw <nahueldeleeuw@hotmail.com>
2021-11-04 09:26:50 -0300

    Second commit

--- js/scm_main_page_script.js
+++ js/scm_main_page_script.js
@@ -5,7 +5,8 @@ window.addEventListener("load", function isListingsElementAvailable() {
     };
 });
 
-function addListingCheckboxes() { //Add checkboxes for active listings
+//Add checkboxes for active listings
+function addListingCheckboxes() { 
     const listingRowElements = document.getElementById("tabContentsMyActiveMarketListingsRows").children;
     for(let i = 0; i < listingRowElements.length; i++) {
         let checkbox = document.createElement("input");
@@ -33,7 +34,7 @@ function wereCheckboxesAdded() {
 };
 
 //Add the extension's elements
-function addExtensionElements() {
+function addExtensionElements() { 
     addListingCheckboxes();
 
     const listingsContainer = document.getElementById("tabContentsMyActiveMarketListingsRows");
@@ -116,29 +117,46 @@ function addExtensionElements() {
     let searchBar = document.createElement("input");
     searchBar.type = "text";
     searchBar.id = "bGoneSearchBar";
-    searchBar.className = "bGoneInputField";
+    searchBar.className = "bGoneInputBar";
     searchBar.placeholder = "Enter the item you wish to remove";
     searchBarContainer.append(searchBar);
 
     let searchBarButton = document.createElement("button");
     searchBarButton.id = "bGoneSearchBarButton";
     searchBarButton.textContent = "Remove";
-    searchBarButton.onclick = getMarketListings;
+    searchBarButton.onclick = startSearch;
     searchBarContainer.append(searchBarButton);
 
     let priceInputBarContainer = document.createElement("div");
     priceInputBarContainer.className = "bGoneInputContainer";
     bGoneSearchBarContainer.append(priceInputBarContainer);
 
-    let priceInputBar = document.createElement("input");
-    priceInputBar.type = "number";
-    priceInputBar.id = "bGonePriceInputBar";
-    priceInputBar.className = "bGoneInputField";
-    priceInputBar.placeholder = "Enter the price of the items you want to remove";
-    priceInputBarContainer.append(priceInputBar);
-
     let priceInputSelector = document.createElement("select");
-    priceInputSelector.id = "priceInputSelector";
+    priceInputSelector.id = "bGonePriceInputSelector";
+
+    let priceInputBar = document.createElement("input");
+    priceInputBar.type = "text";
+    priceInputBar.id = "bGonePriceInputBar";
+    priceInputBar.className = "bGoneInputBar";
+    priceInputBar.placeholder = "Enter the price of the items you want to remove";
+    //priceInputBar.onblur = checkPriceInput; //Check only when about to search
+    priceInputBar.onkeydown = function(event) {
+        let selectorValue = document.getElementById("bGonePriceInputSelector").value;
+        let validInputs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'Backspace'];
+    
+        if(selectorValue === '3') {
+            validInputs.push('-');
+
+            if(!validInputs.includes(event.key)) {
+                event.preventDefault();
+            };
+        } else {
+            if(!validInputs.includes(event.key)) {
+                event.preventDefault();
+            };
+        }
+    };
+    priceInputBarContainer.append(priceInputBar);
     priceInputBarContainer.append(priceInputSelector);
 
     let priceInputEqual = document.createElement("option");
@@ -165,19 +183,365 @@ function addExtensionElements() {
     quantityInputContainer.className = "bGoneInputContainer";
     bGoneSearchBarContainer.append(quantityInputContainer);
 
+    let quantityInputBar = document.createElement("input");
+    quantityInputBar.type = "text";
+    quantityInputBar.id = "bGoneQuantityInput";
+    quantityInputBar.className = "bGoneInputBar";
+    quantityInputBar.style.display = "block";
+    quantityInputBar.placeholder = "Enter the quantity of matching listings you want to remove";
+    //quantityInputBar.onblur = checkQuantityInput; //Check only when about to search
+    quantityInputBar.onkeydown = function(event) {
+        let validInputs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace'];
+    
+        if(!validInputs.includes(event.key)) {
+            event.preventDefault();
+        };
+    };
+    quantityInputContainer.append(quantityInputBar);
+};
 
-    let quantityInput = document.createElement("input");
-    quantityInput.type = "number";
-    quantityInput.id = "bGoneQuantityInput";
-    quantityInput.className = "bGoneInputField";
-    quantityInput.style.display = "block";
-    quantityInput.placeholder = "Enter the quantity of listings you want to remove";
-    quantityInputContainer.append(quantityInput);
+function checkPriceInput() {
+    let element = document.getElementById("bGonePriceInputBar");
+    let selectorValue = document.getElementById("bGonePriceInputSelector").value;
+    let value;
+
+    if(element.value === '') {
+        return true;
+    };
+
+    if(selectorValue !== '3') {
+        value = removeCommas(element.value);
+        value = Number.parseFloat(value);
+
+        if(!Number.isNaN(value) && value > 0) {
+            element.value = value;
+            return true;
+        } else {
+            element.value = "";
+            return false;
+        };
+    } else {
+        value = element.value;
+
+        value = value.split("-");
+        if(value.length !== 2) {
+            element.value = "";
+            return false;
+        }
+
+        for(let index = 0; index < 2; index++) {
+            value[index] = removeCommas(value[index]);
+            
+            for(let i = 0; i < value[index].length; i++) {
+                if(isNaN(value[index].charAt(i)) && value[index].charAt(i) !== '.') {
+                    element.value = "";
+                    return false;
+                };
+            };
+
+            value[index] = Number.parseFloat(value[index]);
+        };
+
+        if(value[0] == value[1]) {
+            return false;
+        };
+
+        if(value[0] > value[1]) {
+            let maxValue = value[0];
+            value[0] = value[1];
+            value[1] = maxValue;
+        };
+
+        element.value = value[0] + "-" + value[1];
+        return true;
+    };
+};
+
+function removeCommas(val) { //If a comma is found that means dots are used as a thousand separator, we don't want that.
+    for(let i = 0; i < val.length; i++) {
+        if(val.charAt(i) === ",") {
+            val = val.replace(/\./g, "");
+            val = val.replace(/\,/g, ".");
+            break;
+        };
+    };
+
+    return val;
+};
+
+function checkQuantityInput() { //When function to get listings count is done use here to check (inputValue <= Count)?
+    let element = document.getElementById("bGoneQuantityInput");
+
+    if(element.value === '') {
+        return true;
+    };
+
+    let value = Number.parseInt(element.value);
+
+    if(!Number.isNaN(value) && value > 0) {
+        element.value = value;
+        return true;
+    } else {
+        element.value = '';
+        return false;
+    };
 };
 
 function getItemId(listingElementId) {
-    //Function input example: mylisting_4060547854836020987
+    //Function input example: "mylisting_4060547854836020987"
     return listingElementId.substring(10, listingElementId.length);
+};
+
+function startSearch(prevStartValue) {
+    let nameInput = document.getElementById("bGoneSearchBar").value;
+    let priceInput = document.getElementById("bGonePriceInputBar").value;
+
+    if(nameInput === "" && priceInput === "") {
+        alert("Can't perform search with both item name and price empty.");
+        document.getElementById("bGonePriceInputBar").value = '';
+        return false;
+    } else {
+        //Input check.
+        let isPriceValid = checkPriceInput();
+        
+        if(!isPriceValid) {
+            let priceSearchMode = document.getElementById("bGonePriceInputSelector").value;
+
+            if(priceSearchMode === '3') {
+                alert('Input price range is not valid.\nFirst write the minimum value, then a dash (-) and then the maximum value.\nExample: 1.05-3');
+                return false;
+            } else {
+                alert('Input price is not valid.\nOnly values over 0 are accepted.\nExample: 100.25');
+                return false;
+            };
+        };
+
+        let isQuantityValid = checkQuantityInput();
+
+        if(!isQuantityValid) {
+            alert('Input quantity is not valid.\nOnly integers over 0 are accepted.\nExample: 20');
+            return false;
+        };
+    };
+
+    console.log("c busca");
+    return;//----------------------------------------------------------------------------------------------
+    if(typeof prevStartValue !== "number" || prevStartValue === null) {
+        lockInputs();
+        getMarketListings(0, 100);
+        return;
+    };
+
+    let listingsAmount = Number.parseInt(document.getElementById("my_market_activelistings_number").innerHTML);
+
+    if(listingsAmount > (prevStartValue + 100)) {
+        getMarketListings(prevStartValue + 100, 100);
+    } else {
+        console.log("Finished search.");
+        unlockInputs();
+    };
+};
+
+function lockInputs() {
+    let searchBar = document.getElementById("bGoneSearchBar");
+    searchBar.setAttribute("disabled", true);
+
+    let priceInputBar = document.getElementById("bGonePriceInputBar");
+    priceInputBar.setAttribute("disabled", true);
+
+    let priceInputSelector = document.getElementById("bGonePriceInputSelector");
+    priceInputSelector.setAttribute("disabled", true);
+
+    let quantityInput = document.getElementById("bGoneQuantityInput");
+    quantityInput.setAttribute("disabled", true);
+};
+
+function unlockInputs() {
+    let searchBar = document.getElementById("bGoneSearchBar");
+    searchBar.disabled = false;
+    searchBar.value = "";
+
+    let priceInputBar = document.getElementById("bGonePriceInputBar");
+    priceInputBar.disabled = false;
+    priceInputBar.value = "";
+
+    let priceInputSelector = document.getElementById("bGonePriceInputSelector");
+    priceInputSelector.disabled = false;
+
+    let quantityInput = document.getElementById("bGoneQuantityInput");
+    quantityInput.disabled = false;
+    quantityInput.value = "";
+};
+
+function getMarketListings(start, count) {
+    if(start === undefined) {
+        start = 0;
+    };
+
+    if(count === undefined) {
+        count = 10;
+    };
+
+    let httpRequest = new XMLHttpRequest();
+
+    httpRequest.onload = function() {
+        if(httpRequest.status === 200) {
+            parseMarketListingsData(httpRequest.responseText, start);
+        } else {
+            console.log("Something happened and couldn't get requested listings data.\nRequest status=" + httpRequest.status);
+        };
+    };
+
+    //Use: https://steamcommunity.com/market/mylistings/?query=&start=0&count=
+    httpRequest.open("GET", "https://steamcommunity.com/market/mylistings/?start=" + start + "&count=" + count, true);
+
+    httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    httpRequest.send();
+};
+
+function parseMarketListingsData(data, startVal) {
+    let listingsDataJSON;
+    let listingRowElements;
+    const listingsData = [];
+
+    if(data !== "null" && data.length > 0) {
+        listingsDataJSON = JSON.parse(data);
+    } else {
+        //JSON was null
+        alert("JSON was null, couldn't search listings");
+        return;
+    }
+
+    if(listingsDataJSON.hasOwnProperty("results_html")) {
+        let activeListingsTable = document.createElement("div");
+        activeListingsTable.innerHTML = listingsDataJSON["results_html"];
+
+        let querySelectorString = "div > div#tabContentsMyActiveMarketListingsTable > div#tabContentsMyActiveMarketListingsRows";
+
+        if(activeListingsTable.querySelector(querySelectorString)) {
+            listingRowElements = activeListingsTable.querySelector(querySelectorString).children;
+        };
+    } else {
+        alert("Field with listing data could not be found");
+        return;
+    };
+
+    //Need: id, name, price
+    for(let i = 0; i < listingRowElements.length; i++){
+        listingsData.push( {
+            id: getItemId(listingRowElements[i].id),
+            name: listingRowElements[i].querySelector("a.market_listing_item_name_link").innerHTML,
+            price: getRawPrice(listingRowElements[i].querySelector("span.market_listing_price > span > span:first-child").innerHTML)
+        } );
+    };
+    
+    console.log(listingsData);
+    searchMatchingListings(listingsData, startVal);
+};
+
+function getRawPrice(price) {
+    let rawPrice = (price.match(/[0-9\,\.]/g)).join("");
+
+    rawPrice = removeCommas(rawPrice);
+
+    rawPrice = Number.parseFloat(rawPrice);
+
+    return rawPrice;
+};
+
+function searchMatchingListings(listingsData, startVal) {
+    let name = document.getElementById("bGoneSearchBar").value;
+    let price = document.getElementById("bGonePriceInputBar").value;
+    let maxPrice;
+    let priceSearchMode = document.getElementById("bGonePriceInputSelector").value;
+    let matchingListingsQuantity = Number.parseInt(document.getElementById("bGoneQuantityInput").value);
+
+    if(priceSearchMode === '3') {
+        let invalidInput = true;
+
+        for(let i = 0; i < price.length; i++) {
+            if(price.charAt(i) === '-') {
+                invalidInput = false;
+            };
+        };
+
+        let priceRange = price.split('-');
+
+        if(priceRange.length !== 2) {
+            invalidInput = true;
+        };
+
+        if(invalidInput) {
+            //Add alert or some better feedback.
+            console.log("Invalid range input");
+            return;
+        };
+
+        price = getRawPrice(priceRange[0]);
+        maxPrice = getRawPrice(priceRange[1]);
+        //Should be good enough for now
+    } else {
+        price = Number.parseFloat(price);
+    };
+
+    for(let i = 0; i < listingsData.length; i++) {
+
+        if(checkName(name, listingsData[i].name) && checkPrice(price, listingsData[i].price, priceSearchMode, maxPrice)) {
+            console.log(listingsData[i].name + "   " + listingsData[i].price + "   " + listingsData[i].id);
+            //removeItemListing(listingsData[i].id, null);
+
+            if(!Number.isNaN(matchingListingsQuantity)) {
+                if(matchingListingsQuantity > 1) {
+                    matchingListingsQuantity--;
+                } else {
+                    let listingsNumber = Number.parseInt(document.getElementById("my_market_activelistings_number").innerHTML);
+                    startSearch(listingsNumber);
+                    return;
+                };
+            };
+        };
+    };
+    //End of func--------------------------------------------------------------------------De-comment when ready to go
+    if(!Number.isNaN(matchingListingsQuantity)) {
+        let quantityInput = document.getElementById("bGoneQuantityInput");
+        quantityInput.value = matchingListingsQuantity;
+    };
+    //startSearch(startVal);
+};
+
+function checkName(nameParam, itemName) {
+    if(nameParam === "") {
+        return true;
+    };
+
+    return nameParam == itemName;
+};
+
+function checkPrice(priceParam, price, mode, maxPrice) {
+    if(Number.isNaN(priceParam) || priceParam === "") {
+        return true;
+    }
+
+    /*
+    0 - Equal to
+    1 - More than
+    2 - Less than
+    3 - Range
+    */
+    switch (mode) {
+        case '0':
+            return priceParam == price;
+        case '1':
+            return priceParam < price;
+        case '2':
+            return priceParam > price;
+        case '3':
+            return (priceParam > price) && (priceParam < maxPrice);
+        default:
+            console.log("Using default price search mode for some reason.");
+            return price == priceParam;
+    };
 };
 
 function removeItemListing(listingId, checkboxElement) {
@@ -186,8 +550,6 @@ function removeItemListing(listingId, checkboxElement) {
     httpRequest.onload = function() {
 
         if(httpRequest.status === 200) {
-            console.log(checkboxElement !== null);
-            console.log(checkboxElement);
             if(checkboxElement !== null) {
                 checkboxElement.checked = false;
             };
@@ -230,67 +592,4 @@ function getSessionIdCookie() {
     };
 
     return cookies.substring(cookieFirstChar, cookieLastChar);
-};
-
-function getMarketListings() {
-    let httpRequest = new XMLHttpRequest();
-    let listingsNumber = document.getElementById("my_market_activelistings_number").innerHTML;
-
-    httpRequest.onload = function queseyo() {
-        parseMarketListingsData(httpRequest.responseText);
-    };
-    //https://steamcommunity.com/market/mylistings/render/
-    //query: '', start: 60, count: 10
-    //?query=&start=0&count=
-    httpRequest.open("GET", "https://steamcommunity.com/market/mylistings/?start=400", true);
-
-    httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    httpRequest.send();
-};
-
-function parseMarketListingsData(data) {
-    let listingsDataJSON;
-    let listingRowElements;
-    const listingsData = [];
-
-    if(data !== "null" && data.length > 0) {
-        listingsDataJSON = JSON.parse(data);
-    } else {
-        //JSON was null
-        alert("JSON was null, couldn't search listings");
-        return;
-    }
-
-    if(listingsDataJSON.hasOwnProperty("results_html")) {
-        let activeListingsTable = document.createElement("div");
-        activeListingsTable.innerHTML = listingsDataJSON["results_html"];
-
-        let querySelectorString = "div > div#tabContentsMyActiveMarketListingsTable > div#tabContentsMyActiveMarketListingsRows";
-
-        if(activeListingsTable.querySelector(querySelectorString)) {
-            listingRowElements = activeListingsTable.querySelector(querySelectorString).children;
-        };
-    } else {
-        alert("Field with listing data could not be found");
-        return;
-    };
-
-    //id, name, price
-    for(let i = 0; i < listingRowElements.length; i++){
-        listingsData.push( {
-            id: getItemId(listingRowElements[1].id),
-            name: listingRowElements[1].querySelector("a.market_listing_item_name_link").innerHTML,
-            price: getRawPrice(listingRowElements[1].querySelector("span.market_listing_price > span > span:first-child").innerHTML)
-        } );
-    };
-
-    return listingsData;
-};
-
-function getRawPrice(price) {
-    let rawPrice;
-
-    console.log(price.replace(/\D*/, ""));
-    return 27;
 };
