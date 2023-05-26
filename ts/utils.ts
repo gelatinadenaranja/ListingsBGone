c13064683a74b8409c612e18a5a6618d3e16eb2d
gelatinadenaranja <nahueldeleeuw@hotmail.com>
2023-05-26 20:29:11 -0300

    Comments clean-up

--- ts/utils.ts
+++ ts/utils.ts
@@ -164,3 +164,22 @@ export function hideLoadingIcon() : void {
 export function showPopUp(message : string) : void {
     alert(message);
 };
+
+/*function setActiveListingPagingPage(targetIndex) {
+    let pagingElements = [...document.getElementById('tabContentsMyActiveMarketListings_links').children];
+    let currentActivePage
+    let targetActivePage;
+
+    for(let i = 0; i < pagingElements.length; i++) {
+        if(pagingElements[i].classList.contains('active')) currentActivePage = pagingElements[i];
+        if(pagingElements[i].textContent === `${targetIndex} `) targetActivePage = pagingElements[i];
+    };
+
+    if(targetActivePage) {
+        currentActivePage.classList.remove('active');
+        targetActivePage.classList.add('active');
+        return true;
+    } else {
+        return false;
+    };
+};*/
