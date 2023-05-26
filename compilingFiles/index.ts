4da7b896b5ce765d00f415ed5b1ffc063ab75689
gelatinadenaranja <nahueldeleeuw@hotmail.com>
2023-05-26 19:57:38 -0300

    Clean up

--- compilingFiles/index.ts
+++
@@ -1,6 +0,0 @@
-
-
-window.addEventListener('load', function isListingsElementAvailable(): void {
-    //Check if there are active listings for the extension to work with.
-    if(document.getElementById('tabContentsMyActiveMarketListingsRows')) addExtensionElements();
-});
